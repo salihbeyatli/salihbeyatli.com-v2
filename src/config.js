6ca1470e9f3631accf768b2dbd7d906e9d6b3331
Salih Beyatlı <salihbeyatli@gmail.com>
2023-04-25 02:22:42 +0300

    den

--- src/config.js
+++ src/config.js
@@ -1,26 +1,22 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'dev@salihbeyatli.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://github.com/salihbeyatli',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/salihbeyatl%C4%B1/',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://codepen.io/slhbytl',
+    },
+    {
+      name: 'Behance',
+      url: 'https://www.behance.net/salihbeyatli',
     },
   ],
 
