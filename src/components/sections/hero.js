4f53441c5254afcea312314d219cf829c10d57c2
Salih Beyatlı <salihbeyatli@gmail.com>
2023-05-01 02:20:51 +0300

    hero update

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,20 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Salih Beyatlı.</h2>;
+  const three = <h3 className="big-heading">I create something.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+      I am developing myself in the fields of front-end and UI / UX. 
+      I aim to create responsive websites that are both user-friendly and visually appealing. 
+      I share my projects on{' '}
+        <a href="https://github.com/salihbeyatli" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+      and{' '}
+        <a href="https://behance.com/salihbeyatli" target="_blank" rel="noreferrer">
+          Behance
         </a>
         .
       </p>
@@ -81,7 +85,7 @@ const Hero = () => {
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my resume!
     </a>
   );
 
