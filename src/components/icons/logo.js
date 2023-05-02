186359463128d7c46bf1807a3758dc6cdfdba6c2
Salih Beyatlı <salihbeyatli@gmail.com>
2023-05-02 07:25:36 +0300

    loader and logo fix

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -4,7 +4,7 @@ const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
     <title>Logo</title>
     <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
+      <g transform="translate(20.000000, 29.000000)">
         <polygon
           id="Shape"
           stroke="currentColor"
