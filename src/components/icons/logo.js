ac73cc43f0e2a27d5608adecfc3837588de36412
Salih Beyatlı <salihbeyatli@gmail.com>
2023-05-02 07:02:22 +0300

    featured bug

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -19,7 +19,7 @@ const IconLogo = () => (
         />
       </g>
     </g>
-    <text text-anchor="start" font-family="Calibre,system,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,Segoe UI,Arial,sans-serif" font-size="60" y="62" x="21" strokewidth="4" stroke="#64FFDA" fill="#64FFDA">
+    <text text-anchor="start" font-family="Calibre,system,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,Segoe UI,Arial,sans-serif" font-size="50" y="62" x="21" strokewidth="4" stroke="#64FFDA" fill="#64FFDA">
     S
     </text>
   </svg>
